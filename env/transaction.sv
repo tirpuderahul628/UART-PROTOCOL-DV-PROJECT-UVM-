020d008a14230aa916931ad4cc67479b72ed801c
tirpuderahul628 <tirpuderahul628@gmail.com>
2026-08-27 00:43:34 +0530

    Add baud rate options 115200 and 128000

--- env/transaction.sv
+++ env/transaction.sv
@@ -13,7 +13,7 @@ class transaction extends uvm_sequence_item;
     logic [7:0] rx_out;
 
     constraint baud_c{
-        baud inside{4800,9600,14400,19200,38400,57600};
+        baud inside{4800,9600,14400,19200,38400,57600,115200,128000};
     }
 
     constraint length_c{
